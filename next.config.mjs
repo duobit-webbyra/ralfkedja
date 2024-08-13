59016b00361f9d4c6aa9ed0d4ffc74fc58ccb597
Marwin Hormiz <marwinhormiz@duobit.se>
2024-08-13 13:39:51 +0200

    configured sass in nextjs config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,17 @@
 import { withPayload } from '@payloadcms/next/withPayload'
 
+import path from 'path'
+const __dirname = process.cwd()
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src', 'app', 'styles')],
+  },
+  experimental: {
+    reactCompiler: true,
+  },
 }
 
 export default withPayload(nextConfig)
