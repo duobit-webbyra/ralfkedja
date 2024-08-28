d47c6b7ef22967f109a05dc85ae7e137c0c104fd
Marwin Hormiz <marwinhormiz@duobit.se>
2024-08-28 22:53:37 +0200

    global contact config

---
+++ src/payload/globals/contact.ts
@@ -0,0 +1,38 @@
+import { GlobalConfig } from 'payload';
+
+export const Contact: GlobalConfig = {
+  slug: 'contact',
+  fields: [
+    {
+      name: 'address',
+      type: 'group',
+      fields: [
+        {
+          name: 'street',
+          type: 'text',
+          required: true,
+        },
+        {
+          name: 'city',
+          type: 'text',
+          required: true,
+        },
+        {
+          name: 'zipcode',
+          type: 'number',
+          required: true,
+        },
+      ],
+    },
+    {
+      name: 'phone',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'email',
+      type: 'email',
+      required: true,
+    },
+  ],
+};
