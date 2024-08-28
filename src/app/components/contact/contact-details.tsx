b023f16c3c186e806136417c60ed7d9931c913f0
Marwin Hormiz <marwinhormiz@duobit.se>
2024-08-28 22:58:55 +0200

    updated contact details to get contact props

--- src/app/components/contact/contact-details.tsx
+++ src/app/components/contact/contact-details.tsx
@@ -3,7 +3,13 @@ import style from './contact-details.module.scss';
 import Facebook from '../graphics/facebook';
 import Instagram from '../graphics/instagram';
 import Link from 'next/link';
-export default function ContactDetails() {
+import { Contact } from '@/payload-types';
+
+interface ContactDetailsProps {
+  data?: Contact;
+}
+
+export default function ContactDetails({ data }: ContactDetailsProps) {
   return (
     <div className={style.container}>
       <div className={style.content}>
@@ -18,18 +24,20 @@ export default function ContactDetails() {
           <div className={style.detail}>
             <h3>Adress</h3>
             <div>
-              <p>Bruksgatan 8B</p>
-              <p>633 20, Eskilstuna</p>
+              <p>{data?.address.street}</p>
+              <p>
+                {data?.address.zipcode}, {data?.address.street}
+              </p>
             </div>
           </div>
           <div className={style.detail}>
             <h3>E-mail</h3>
-            <p>ralked@hotmail.com</p>
+            <p>{data?.email}</p>
           </div>
           <div className={style.detail}>
             <h3>Telefon</h3>
             <div style={{ display: 'flex', flexDirection: 'column', gap: '0.5rem' }}>
-              <p>0709-12 21 28</p>
+              <p>{data?.phone}</p>
               <div style={{ display: 'flex', gap: '0.5rem' }}>
                 <Link href={`${data?.socials?.facebook}`}>
                   <Facebook />
