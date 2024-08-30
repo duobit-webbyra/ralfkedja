d0c31ced66e4577042ebfd6d66ba9e4c98f67c16
Marwin Hormiz <marwinhormiz@duobit.se>
2024-08-30 13:28:57 +0200

    removed unused imports and added contact page component

--- src/app/(app)/kontakt/page.tsx
+++ src/app/(app)/kontakt/page.tsx
@@ -1,9 +1,7 @@
 import ContactDetails from '@/app/components/contact/contact-details';
-import ContactForm from '@/app/components/contact/contact-form';
-import Curve from '@/app/components/graphics/curve';
+import ContactPage from '@/app/components/contact/contact-page';
 import DefaultHero from '@/app/components/hero/default-hero';
 import Map from '@/app/components/utils/map';
-import Title from '@/app/components/utils/title';
 import getContactData from '@/app/utils/get-contact-data';
 
 export default async function Page() {
@@ -20,24 +18,10 @@ export default async function Page() {
       <section
         style={{
           backgroundColor: 'var(--primary-300)',
+          paddingBottom: '4rem',
         }}
       >
-        <div style={{ width: '100%' }}>
-          <Curve fillColor='var(--tertiary-100)' />
-        </div>
-        <Title
-          heading='Hör av dig'
-          subHeading='Fyll i formuläret'
-          description='Jag ser fram emot att höra från dig!'
-          inverse
-        />
-        <div
-          style={{
-            width: 'var(--content-width)',
-          }}
-        >
-          <ContactForm layout='grid' />
-        </div>
+        <ContactPage />
       </section>
       <div style={{ width: '100%', height: '500px' }}>
         <Map />
