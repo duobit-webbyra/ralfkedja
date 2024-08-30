7c8cc39de0d1ba6ba4fc14bb0b25d0c74302bd3b
kevinhormiz <kevinhormiz@duobit.se>
2024-08-30 16:06:52 +0200

    added attribution in footer

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -1,5 +1,6 @@
 import style from './footer.module.scss';
-
+import Link from 'next/link';
+import Image from 'next/image';
 import { MdOutlineLocationOn } from 'react-icons/md';
 import { MdOutlinePhone } from 'react-icons/md';
 import { MdOutlineMail } from 'react-icons/md';
@@ -9,50 +10,64 @@ export default function HeaderInfo() {
   return (
     <div className={style.container}>
       <div className={style.content}>
-        <div className={style.namecopyright}>
-          <p style={{ fontSize: 'var(--text-md)' }}>Ralf Kedja</p>
-          <p
-            style={{
-              fontSize: 'var(--text-xs)',
-              display: 'flex',
-              gap: '4px',
-              alignItems: 'center',
-              color: 'var(--primary-100)',
-            }}
-          >
-            Copyright <FaRegCopyright /> 2024
-          </p>
-        </div>
-
-        <div className={style.info}>
-          <div className={style.item}>
-            <MdOutlineLocationOn />
-            <div
+        <div className={style.inforow}>
+          <div className={style.namecopyright}>
+            <p style={{ fontSize: 'var(--text-md)' }}>Ralf Kedja</p>
+            <p
               style={{
+                fontSize: 'var(--text-xs)',
                 display: 'flex',
-                flexDirection: 'column',
-                lineHeight: '1.25',
+                gap: '4px',
+                alignItems: 'center',
+                color: 'var(--primary-100)',
               }}
             >
-              <span>Bruksgatan 8B</span>
-              <span
+              Copyright <FaRegCopyright /> 2024
+            </p>
+          </div>
+
+          <div className={style.info}>
+            <div className={style.item}>
+              <MdOutlineLocationOn />
+              <div
                 style={{
-                  color: 'var(--primary-100)',
-                  fontSize: 'var(--text-xs)',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  lineHeight: '1.25',
                 }}
               >
-                632 20, Eskilstuna
-              </span>
+                <span>Bruksgatan 8B</span>
+                <span
+                  style={{
+                    color: 'var(--primary-100)',
+                    fontSize: 'var(--text-xs)',
+                  }}
+                >
+                  632 20, Eskilstuna
+                </span>
+              </div>
+            </div>
+            <div className={style.item}>
+              <MdOutlinePhone transform='rotate(10)' />
+              <span>010-0000000</span>
+            </div>
+            <div className={style.item}>
+              <MdOutlineMail />
+              <span>ralked@hotmail.com</span>
             </div>
           </div>
-          <div className={style.item}>
-            <MdOutlinePhone transform='rotate(10)' />
-            <span>010-0000000</span>
-          </div>
-          <div className={style.item}>
-            <MdOutlineMail />
-            <span>ralked@hotmail.com</span>
-          </div>
+        </div>
+        <div className={style.attribution}>
+          <p>Sida producerad av</p>
+          <Link href='https://duobit.se'>
+            <Image
+              src='/duobit-logo-inverted.svg'
+              width={100}
+              height={100}
+              alt='Duobit Webbyrå'
+              className={style.logo}
+            />
+          </Link>
         </div>
       </div>
     </div>
