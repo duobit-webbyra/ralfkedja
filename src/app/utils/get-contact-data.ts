c8109ce979030951c0da30010b3255b832d4d665
Marwin Hormiz <marwinhormiz@duobit.se>
2024-08-30 12:38:28 +0200

    changed name of function

--- src/app/utils/get-contact-data.ts
+++ src/app/utils/get-contact-data.ts
@@ -2,7 +2,7 @@ import { getPayloadHMR } from '@payloadcms/next/utilities';
 import config from '@payload-config';
 import { Contact } from '@payload-types';
 
-export default async function GetContactData() {
+export default async function getContactData() {
   const payload = await getPayloadHMR({ config });
 
   const data: Contact = await payload.findGlobal({
