fce0111f4bb1eab498205d233581f9cf5fe9dadd
Marwin Hormiz <marwinhormiz@duobit.se>
2024-08-30 13:14:40 +0200

    removed unused code

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -5,8 +5,6 @@ import Curve from '../components/graphics/curve';
 import ContactMe from '../components/home/contact-me';
 import AboutMeOverview from '../components/home/about-me';
 import Announcement from '../components/announcement/announcement';
-import ContactForm from '../components/contact/contact-form';
-import Map from '../components/utils/map';
 
 export default function Page() {
   return (
@@ -15,20 +13,6 @@ export default function Page() {
         <LandingPageHero />
       </section>
       <Announcement />
-      {/* <section */}
-      {/*   style={{ */}
-      {/*     backgroundColor: 'var(--primary-300)', */}
-      {/*     padding: '1rem 0', */}
-      {/*     width: '100%', */}
-      {/*     display: 'flex', */}
-      {/*     justifyContent: 'center', */}
-      {/*     color: 'var(--secondary-100)', */}
-      {/*     fontSize: '1.5rem', */}
-      {/*     textAlign: 'center', */}
-      {/*   }} */}
-      {/* > */}
-      {/*   29/08 är jag borta på semester och kommer tillbaka 06/09 */}
-      {/* </section> */}
       <section
         style={{
           paddingBlock: '4rem',
