19d9641d547dd8f523fec6f609fcd8d0e885afdb
Marwin Hormiz <marwinhormiz@duobit.se>
2024-09-03 18:35:19 +0200

    changed name to GalleryGrid

--- src/app/(app)/galleri/page.tsx
+++ src/app/(app)/galleri/page.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Gallery from '@/app/components/gallery/gallery';
+import GalleryGrid from '@/app/components/gallery/gallery';
 import DefaultHero from '@/app/components/hero/default-hero';
 export default function page() {
   return (
     <>
       <DefaultHero title='Galleri' />
       <section style={{ paddingBlock: '4rem' }}>
-        <Gallery />
+        <GalleryGrid />
       </section>
     </>
   );
