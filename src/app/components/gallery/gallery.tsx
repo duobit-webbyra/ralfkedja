832f128e4671be78733aab92684691766ac9e3d2
Marwin Hormiz <marwinhormiz@duobit.se>
2024-09-03 11:27:56 +0200

    restyled gallery page

--- src/app/components/gallery/gallery.tsx
+++ src/app/components/gallery/gallery.tsx
@@ -1,59 +1,36 @@
-'use client';
 import React, { useEffect, useState } from 'react';
 import style from './gallery.module.scss';
 import Image from 'next/image';
+import config from '@payload-config';
+import { getPayloadHMR } from '@payloadcms/next/utilities';
+import type { Media } from '@payload-types';
 
-interface ImageProps {
-  image: {
-    url: string;
-  };
-}
+const GalleryGrid = async () => {
+  const payload = await getPayloadHMR({ config });
+  const { docs: data } = await payload.find({
+    collection: 'gallery',
+  });
 
-interface GalleryItem {
-  title: string;
-  images: ImageProps[];
-}
-
-const Gallery = () => {
-  const [galleryItems, setGalleryItems] = useState<GalleryItem[]>([]);
-
-  useEffect(() => {
-    const fetchGalleryItems = async () => {
-      try {
-        const response = await fetch('/api/gallery'); // Adjust the API endpoint as needed
-        const data = await response.json();
-        if (data && data.docs) {
-          setGalleryItems(data.docs);
-        } else {
-          console.error('Invalid data structure:', data);
-        }
-      } catch (error) {
-        console.error('Error fetching gallery items:', error);
-      }
-    };
-
-    fetchGalleryItems();
-  }, []);
+  console.log(data);
 
   return (
     <div className={style.container}>
-      {galleryItems.length > 0 ? (
-        galleryItems.map((item, index) => (
-          <div key={index} className={style.galleryItem}>
-            <h2>{item.title}</h2>
-            <div className={style.images}>
-              {item.images.map((img, imgIndex) => (
+      {data.length > 0 ? (
+        <div className={style['gallery-container']}>
+          {data.map((item, index) => (
+            <div key={index} className={style['gallery-item']}>
+              <div className={style.images}>
                 <Image
-                  key={imgIndex}
-                  src={img.image.url}
+                  key={index}
+                  src={`${(item.images as Media).url}`}
                   alt={item.title}
-                  width={200}
-                  height={200}
+                  fill
+                  style={{ objectFit: 'cover' }}
                 />
-              ))}
+              </div>
             </div>
-          </div>
-        ))
+          ))}
+        </div>
       ) : (
         <p>Inga bilder uppladdade för tillfället</p>
       )}
@@ -61,4 +38,4 @@ const Gallery = () => {
   );
 };
 
-export default Gallery;
+export default GalleryGrid;
