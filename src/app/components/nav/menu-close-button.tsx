7b64f6d453c78100fe0f2dd880c3eac09c98cd9a
kevinhormiz <kevinhormiz@duobit.se>
2024-09-12 12:13:38 +0200

    fixed svgs

--- src/app/components/nav/menu-close-button.tsx
+++ src/app/components/nav/menu-close-button.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 export default function MenuCloseButton() {
   return (
     <svg viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
-      <g id='SVGRepo_bgCarrier' stroke-width='0'></g>
-      <g id='SVGRepo_tracerCarrier' stroke-linecap='round' stroke-linejoin='round'></g>
+      <g id='SVGRepo_bgCarrier' strokeWidth='0'></g>
+      <g id='SVGRepo_tracerCarrier' strokeLinecap='round' strokeLinejoin='round'></g>
       <g id='SVGRepo_iconCarrier'>
         {' '}
         <path
