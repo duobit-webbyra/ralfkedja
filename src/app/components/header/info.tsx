9e4aba8f19c1434a53e5e5137c78b3905aa9b99c
Marwin Hormiz <marwinhormiz@duobit.se>
2024-09-24 13:59:00 +0200

    added link to logo

--- src/app/components/header/info.tsx
+++ src/app/components/header/info.tsx
@@ -1,11 +1,6 @@
 import style from './info.module.scss';
-
-// import { MdOutlineLocationOn } from 'react-icons/md';
-// import { MdOutlinePhone } from 'react-icons/md';
-// import { MdOutlineMail } from 'react-icons/md';
-
-// import getContactData from '@/app/utils/get-contact-data';
 import ShortInfo from '../utils/short-info';
+import Link from 'next/link';
 
 export default async function HeaderInfo() {
   // const data = await getContactData();
@@ -13,38 +8,10 @@ export default async function HeaderInfo() {
   return (
     <div className={style.container}>
       <div className={style.content}>
-        <p style={{ fontSize: 'var(--text-md)' }}>Ralf Kedja</p>
+        <Link style={{ color: 'var(--primary-400)', fontSize: 'var(--text-md)' }} href='/'>
+          Ralf Kedja
+        </Link>
         <ShortInfo />
-        {/* <div className={style.info}> */}
-        {/*   <div className={style.item}> */}
-        {/*     <MdOutlineLocationOn /> */}
-        {/*     <div */}
-        {/*       style={{ */}
-        {/*         display: 'flex', */}
-        {/*         flexDirection: 'column', */}
-        {/*         lineHeight: '1.25', */}
-        {/*       }} */}
-        {/*     > */}
-        {/*       <span>{data?.address.street}</span> */}
-        {/*       <span */}
-        {/*         style={{ */}
-        {/*           color: 'var(--primary-100)', */}
-        {/*           fontSize: 'var(--text-xs)', */}
-        {/*         }} */}
-        {/*       > */}
-        {/*         {data?.address.zipcode}, {data?.address.city} */}
-        {/*       </span> */}
-        {/*     </div> */}
-        {/*   </div> */}
-        {/*   <div className={style.item}> */}
-        {/*     <MdOutlinePhone transform='rotate(10)' /> */}
-        {/*     <span>{data?.phone}</span> */}
-        {/*   </div> */}
-        {/*   <div className={style.item}> */}
-        {/*     <MdOutlineMail /> */}
-        {/*     <span>{data?.email}</span> */}
-        {/*   </div> */}
-        {/* </div> */}
       </div>
     </div>
   );
