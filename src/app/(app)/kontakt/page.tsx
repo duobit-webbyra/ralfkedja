c449403047ee4ac6f3b36b3c37f96b012857d652
kevinhormiz <kevinhormiz@duobit.se>
2024-10-31 11:35:54 +0100

    replaced map with iframe

--- src/app/(app)/kontakt/page.tsx
+++ src/app/(app)/kontakt/page.tsx
@@ -1,7 +1,6 @@
 import ContactDetails from '@/app/components/contact/contact-details';
 import ContactPage from '@/app/components/contact/contact-page';
 import DefaultHero from '@/app/components/hero/default-hero';
-import Map from '@/app/components/utils/map';
 import getContactData from '@/app/utils/get-contact-data';
 
 import { Metadata } from 'next';
@@ -27,9 +26,13 @@ export default async function Page() {
       >
         <ContactPage />
       </section>
-      <div style={{ width: '100%', height: '500px' }}>
-        <Map />
-      </div>
+      <iframe
+        src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2033.2085446041845!2d16.511617477273603!3d59.36285830833638!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465ef2ba833abc05%3A0x4c54dd0f62152d6d!2sK%C3%B6pmangatan%204B%2C%20633%2056%20Eskilstuna!5e0!3m2!1ssv!2sse!4v1729937955124!5m2!1ssv!2sse'
+        width='100%'
+        height='500'
+        loading='lazy'
+        style={{ border: 0 }}
+      />
     </>
   );
 }
--- src/app/components/home/contact-me.tsx
+++ src/app/components/home/contact-me.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import style from './contact-me.module.scss';
 import Title from '../utils/title';
-import Map from '../utils/map';
 
 import ContactForm from '../contact/contact-form';
 
@@ -33,10 +32,14 @@ export default function ContactMe() {
           <div className={style.form}>
             <ContactForm layout='flex' />
           </div>
-
-          <div className={style.mapcontainer}>
-            <Map />
-          </div>
+          <iframe
+            className={style.mapcontainer}
+            src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2033.2085446041845!2d16.511617477273603!3d59.36285830833638!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465ef2ba833abc05%3A0x4c54dd0f62152d6d!2sK%C3%B6pmangatan%204B%2C%20633%2056%20Eskilstuna!5e0!3m2!1ssv!2sse!4v1729937955124!5m2!1ssv!2sse'
+            width='100%'
+            height='500'
+            loading='lazy'
+            style={{ border: 0 }}
+          />
         </div>
       </div>
     </div>
