7d7d6ef3081b14383c8edc8abb1f9ae3b212189b
kevinhormiz <kevinhormiz@gmail.com>
2025-05-14 19:22:01 +0200

    removed pw

--- src/payload/collections/users.ts
+++ src/payload/collections/users.ts
@@ -3,24 +3,6 @@ import { isHost } from '../access/is-host';
 import { isAdmin } from '../access/is-admin';
 import { ValidationError, CollectionBeforeValidateHook } from 'payload';
 
-const validatePassword: CollectionBeforeValidateHook = ({ data }) => {
-  const password = data?.password;
-
-  let message: string | undefined = undefined;
-  if (!password) message = 'Password is required.';
-  else if (password.length <= 6) message = 'Password must be at least 6 characters long';
-
-  const hasUpperCase = /[A-Z]/.test(password || '');
-  const hasLowerCase = /[a-z]/.test(password || '');
-  const hasNumber = /\d/.test(password || '');
-
-  if (password && (!hasUpperCase || !hasLowerCase))
-    message = 'Password must have both uppercase and lowercase letters';
-  else if (password && !hasNumber) message = 'Password must contain at least one number';
-
-  if (message) throw new ValidationError({ errors: [{ message, path: 'password' }] });
-};
-
 export const Users: CollectionConfig = {
   slug: 'users',
   admin: {
@@ -44,9 +26,7 @@ export const Users: CollectionConfig = {
     maxLoginAttempts: 5,
     lockTime: 600 * 1000, // 10 minutes
   },
-  hooks: {
-    beforeValidate: [validatePassword],
-  },
+
   fields: [
     {
       name: 'name',
