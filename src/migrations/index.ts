9089a3ca0f604dd1ef6356c20615f0e70f080641
kevinhormiz <kevinhormiz@gmail.com>
2025-10-05 11:21:47 +0200

    new migration, old didnt have name for users

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,6 +1,7 @@
 import * as migration_20250929_111647 from './20250929_111647';
 import * as migration_20251004_134335 from './20251004_134335';
 import * as migration_20251004_144351 from './20251004_144351';
+import * as migration_20251005_091906 from './20251005_091906';
 
 export const migrations = [
   {
@@ -16,6 +17,11 @@ export const migrations = [
   {
     up: migration_20251004_144351.up,
     down: migration_20251004_144351.down,
-    name: '20251004_144351'
+    name: '20251004_144351',
+  },
+  {
+    up: migration_20251005_091906.up,
+    down: migration_20251005_091906.down,
+    name: '20251005_091906'
   },
 ];
