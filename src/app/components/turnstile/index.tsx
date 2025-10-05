99dbf7311f307e179b4d397d7d5ab5ae561d4fc4
kevinhormiz <kevinhormiz@gmail.com>
2025-10-05 17:07:03 +0200

    testing string turnstile key

--- src/app/components/turnstile/index.tsx
+++ src/app/components/turnstile/index.tsx
@@ -1,16 +1,16 @@
-'use client';
+'use client'
 
-import Script from 'next/script';
+import Script from 'next/script'
 
 export default function Turnstile() {
   return (
     <>
-      <Script async defer src='https://challenges.cloudflare.com/turnstile/v0/api.js' />
+      <Script async defer src="https://challenges.cloudflare.com/turnstile/v0/api.js" />
 
       <div
-        className='cf-turnstile'
-        data-sitekey={process.env.NEXT_PUBLIC_CLOUDFLARE_TURNSTILE_SITE_KEY}
+        className="cf-turnstile"
+        data-sitekey={String(process.env.NEXT_PUBLIC_CLOUDFLARE_TURNSTILE_SITE_KEY)}
       />
     </>
-  );
+  )
 }
