bc32ac9f8b630e8c68184a1021c430072842431c
kevinhormiz <kevinhormiz@gmail.com>
2025-10-05 15:28:47 +0200

    new migration init

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,27 +1,9 @@
-import * as migration_20250929_111647 from './20250929_111647';
-import * as migration_20251004_134335 from './20251004_134335';
-import * as migration_20251004_144351 from './20251004_144351';
-import * as migration_20251005_091906 from './20251005_091906';
+import * as migration_20251005_132814 from './20251005_132814';
 
 export const migrations = [
   {
-    up: migration_20250929_111647.up,
-    down: migration_20250929_111647.down,
-    name: '20250929_111647',
-  },
-  {
-    up: migration_20251004_134335.up,
-    down: migration_20251004_134335.down,
-    name: '20251004_134335',
-  },
-  {
-    up: migration_20251004_144351.up,
-    down: migration_20251004_144351.down,
-    name: '20251004_144351',
-  },
-  {
-    up: migration_20251005_091906.up,
-    down: migration_20251005_091906.down,
-    name: '20251005_091906'
+    up: migration_20251005_132814.up,
+    down: migration_20251005_132814.down,
+    name: '20251005_132814'
   },
 ];
