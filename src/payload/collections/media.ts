82f91539bc8c6cbb59cf5f3e962721c3cc8a87e4
kevinhormiz <kevinhormiz@gmail.com>
2025-10-10 18:39:52 +0200

    testing out media upload url

--- src/payload/collections/media.ts
+++ src/payload/collections/media.ts
@@ -1,5 +1,5 @@
-import type { CollectionConfig } from 'payload';
-import { isAdmin } from '../access/is-admin';
+import type { CollectionConfig } from 'payload'
+import { isAdmin } from '../access/is-admin'
 
 export const Media: CollectionConfig = {
   slug: 'media',
@@ -15,5 +15,7 @@ export const Media: CollectionConfig = {
       required: true,
     },
   ],
-  upload: true,
-};
+  upload: {
+    staticURL: 'https://pub-93ba30fcaa484171b792d3d22c7ef793.r2.dev',
+  },
+}
