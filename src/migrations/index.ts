c65987e444829c4e73f868aea04f8958776043cc
kevinhormiz <kevinhormiz@gmail.com>
2026-01-18 13:29:54 +0100

    Fix: Remove deleted migration reference from index

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,2 @@
-import * as migration_20251005_135017 from './20251005_135017';
+export const migrations = [];
 
-export const migrations = [
-  {
-    up: migration_20251005_135017.up,
-    down: migration_20251005_135017.down,
-    name: '20251005_135017'
-  },
-];
